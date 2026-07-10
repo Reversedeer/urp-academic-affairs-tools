501e96cf3ad383768cf60c4b7554aa9d961a7af9
Reversedeer <ysjvillmark@gmail.com>
2026-07-11 01:48:35 +0800

    ♻️ 重构 client exports

--- urp_academic_affairs_tools/client/__init__.py
+++ urp_academic_affairs_tools/client/__init__.py
@@ -1,4 +1,28 @@
+from .auth import extract_token_value
 from .api import fetch_tasks, get_this_semester_timetable
-from .session import AsyncJWSSession
+from .captcha import CaptchaRecognizer
+from .errors import (
+    AuthError,
+    InvalidCredentialsError,
+    ServiceError,
+    SessionExpiredError,
+)
+from .session import (
+    AsyncJWSSession,
+    RetryPolicy,
+    SessionOptions,
+)
 
-__all__ = ["AsyncJWSSession", "fetch_tasks", "get_this_semester_timetable"]
+__all__ = [
+    "AsyncJWSSession",
+    "AuthError",
+    "CaptchaRecognizer",
+    "InvalidCredentialsError",
+    "RetryPolicy",
+    "ServiceError",
+    "SessionExpiredError",
+    "SessionOptions",
+    "extract_token_value",
+    "fetch_tasks",
+    "get_this_semester_timetable",
+]
